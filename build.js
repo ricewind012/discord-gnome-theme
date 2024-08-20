2b66606c1efaecad3bd2f962e2c02ad044d894bc
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-08-20 18:49:20 +0000

    use async for the compiler

--- build.js
+++ build.js
@@ -2,23 +2,31 @@ import fs from "node:fs";
 import path from "node:path";
 import * as sass from "sass";
 
-const allFiles = fs.readdirSync("src", { recursive: true });
+const DIST_DIR = "dist";
+const SRC_DIR = "src";
+
+const allFiles = fs.readdirSync(SRC_DIR, { recursive: true });
 const files = allFiles.filter((e) => e.endsWith(".scss"));
 const dirs = allFiles.filter((e) => !e.endsWith(".scss"));
 
-fs.rmSync("dist", { recursive: true });
+fs.rmSync(DIST_DIR, { recursive: true });
 for (const dir of dirs) {
-	fs.mkdirSync(path.join("dist", dir), { recursive: true });
+	fs.mkdirSync(path.join(DIST_DIR, dir), { recursive: true });
 }
 
 for (const file of files) {
-	const { css } = sass.compile(path.join("src", file));
-	const filePath = path.join("dist", file).replace("scss", "css");
+	const { css } = await sass
+		.compileAsync(path.join(SRC_DIR, file))
+		.catch((e) => {
+			console.log(e.message);
+			process.exit(1);
+		});
+	const filePath = path.join(DIST_DIR, file).replace("scss", "css");
 
 	fs.writeFileSync(filePath, css);
 }
 
 fs.writeFileSync(
-	path.join("dist", "index.css"),
+	path.join(DIST_DIR, "index.css"),
 	files.map((e) => `@import "${e.replace("scss", "css")}";`).join("\n"),
 );
