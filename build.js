9dcb4dc8b1979504a5bbc99898691f4e6dc538b8
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-09-07 17:19:26 +0000

    use files for svgs

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
-import * as sass from "sass";
+import { compileAsync, SassString } from "sass";
+import { optimize } from "svgo";
 
 const DIST_DIR = "dist";
 const SRC_DIR = "src";
@@ -9,18 +10,36 @@ const allFiles = fs.readdirSync(SRC_DIR, { recursive: true });
 const files = allFiles.filter((e) => e.endsWith(".scss"));
 const dirs = allFiles.filter((e) => !e.endsWith(".scss"));
 
+function inlineSvg(args) {
+	const arg = args[0].assertString("path");
+	const fileName = `${arg.toString().replace(/"/g, "")}-symbolic.svg`;
+	const filePath = path.join("assets", "icons", fileName);
+	const { data } = optimize(fs.readFileSync(filePath));
+
+	return new SassString(
+		`url("data:image/svg+xml;${encodeURIComponent(data)}")`,
+		{ quotes: false },
+	);
+}
+
 fs.rmSync(DIST_DIR, { recursive: true });
 for (const dir of dirs) {
 	fs.mkdirSync(path.join(DIST_DIR, dir), { recursive: true });
 }
 
 for (const file of files) {
-	const { css } = await sass
-		.compileAsync(path.join(SRC_DIR, file))
-		.catch((e) => {
-			console.log(e.message);
-			process.exit(1);
-		});
+	const { css } = await compileAsync(path.join(SRC_DIR, file), {
+		functions: {
+			"svg($path)": inlineSvg,
+		},
+	}).catch((e) => {
+		console.log(e.message);
+		process.exit(1);
+	});
+	// these just get ignored ???????????? fuck off
+	if (file === "global/icons.scss" || file === "content/header.scss") {
+		console.log(css);
+	}
 	const filePath = path.join(DIST_DIR, file).replace("scss", "css");
 
 	fs.writeFileSync(filePath, css);
